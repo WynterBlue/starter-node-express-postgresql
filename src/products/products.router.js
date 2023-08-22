b9b199bb2e59fa7e2acf624edc14e06069bc63f2
Alex <wynterblue02@gmail.com>
2023-08-22 11:54:13 -0400

    first commit(I should've done this way earlier)

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -3,6 +3,11 @@ const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:productId").get(controller.read).all(methodNotAllowed);
+
+router.route("/:productId([0-9+])").get(controller.read).all(methodNotAllowed);
+
+router.route("/out-of-stock-count").get(controller.listOutOfStockCount);
+
+router.route("/price-summary").get(controller.listPriceSummary)
 
 module.exports = router;
---
+++ src/products/products.service.js
@@ -0,0 +1,33 @@
+const knex = require("../db/connection");
+
+function list() {
+  return knex("products").select("*");
+}
+
+function read(productId) {
+  return knex("products").select("*").where({ product_id: productId }).first();
+}
+
+function listOutOfStockCount(){
+  return knex("products")
+    .select("product_quantity_in_stock as out_of_stock")
+    .count("product_id")
+    .where({ product_quantity_in_stock: 0 })
+    .groupBy("out_of_stock");
+}
+
+function listPriceSummary(){
+  return knex("products")
+    .select("supplier_id")
+    .min("product_price")
+    .max("product_price")
+    .avg("product_price")
+    .groupBy("supplier_id");
+}
+
+module.exports = {
+  list,
+  read,
+  listOutOfStockCount,
+  listPriceSummary,
+};
